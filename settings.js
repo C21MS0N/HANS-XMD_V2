b051ad4858d4cdb0a0de4f359fd879ea637ce9d8
C21MS0N <rendabelrebel2007@gmail.com>
2025-07-19 17:16:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,29 +9,29 @@ const chalk = require('chalk')
 require('dotenv').config()
 
 //contact details - sensitive info moved to .env
-global.SESSION_ID = process.env.SESSION_ID || "" //fallback to default if not in .env
-global.ownernomer = process.env.OWNER_NUMBER || "255614195217"
+global.SESSION_ID = process.env.SESSION_ID || "HansTz&u1h1yBAZ#TFjSA1ZCRNne_y1xW4B9hjzKPDUJF-TVUYxXIuonHJE" //fallback to default if not in .env
+global.ownernomer = process.env.OWNER_NUMBER || "2349075268689"
 
-global.ownername = process.env.OWNER_NAME || "HansTech"
+global.ownername = process.env.OWNER_NAME || "CRIMSON♦️"
 
 global.ytname = "HANSTZ-TECH"
 
 global.socialm = "GitHub: Mrhanstz"
 
-global.location = "AFRICA, TANZANIA, DODOMA"
+global.location = "AFRICA, NIGERIA, LAGOS"
 
-global.ownernumber = process.env.OWNER_NUMBER || '255614195217'  //Owner number
+global.ownernumber = process.env.OWNER_NUMBER || '2349075268689'  //Owner number
 
 global.ownername = process.env.OWNER_NAME || 'HANSTZ' //owner name
-global.botname = 'HANS-XMD' //name of the bot
+global.botname = 'KURIMUZON♦️' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'HANS-XMD'
+global.author = 'CRIMSON'
 
 //console view/theme
-global.themeemoji = '⚠️'
-global.wm = "HANS BOT"
+global.themeemoji = '♦️'
+global.wm = "CRIMSON BOT"
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31'
